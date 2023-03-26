6cacab0ad30fea72ec5618960fd0d3618ab9aafd
pjmeyer95 <pjmeyer95@gmail.com>
2023-03-26 17:02:44 -0600

    initial commit

--- script.js
+++ script.js
@@ -1,52 +1,54 @@
-// get current date
-var currentDate = moment().format("dddd, MMMM Do");
+$(document).ready(function() {
+  // get current date
+  var currentDate = moment().format("dddd, MMMM Do");
 
-// display current date in header
-$("#currentDay").text(currentDate);
+  // display current date in header
+  $("#currentDay").text(currentDate);
 
-// check time and set time block color
-function setTimeBlockColor() {
-  // get current hour
-  var currentHour = moment().hour();
+  // check time and set time block color
+  function setTimeBlockColor() {
+    // get current hour
+    var currentHour = moment().hour();
 
-  // loop through time blocks
+    // loop through time blocks
+    $(".time-block").each(function() {
+      // get time block hour
+      var timeBlockHour = parseInt($(this).children(".hour").text().slice(0, -2));
+
+      // set time block color based on current hour
+      if (timeBlockHour < currentHour) {
+        $(this).children(".description").addClass("past");
+      }
+      else if (timeBlockHour === currentHour) {
+        $(this).children(".description").addClass("present");
+      }
+      else {
+        $(this).children(".description").addClass("future");
+      }
+    });
+  }
+
+  setTimeBlockColor();
+
+  // save events to local storage
+  $(".saveBtn").on("click", function() {
+    // get event description and hour
+    var eventDescription = $(this).siblings(".description").val();
+    var eventHour = $(this).siblings(".hour").text();
+
+    // save event to local storage
+    localStorage.setItem(eventHour, eventDescription);
+  });
+
+  // load events from local storage
   $(".time-block").each(function() {
     // get time block hour
-    var timeBlockHour = parseInt($(this).children(".hour").text().slice(0, -2));
+    var timeBlockHour = $(this).children(".hour").text();
 
-    // set time block color based on current hour
-    if (timeBlockHour < currentHour) {
-      $(this).children(".description").addClass("past");
-    }
-    else if (timeBlockHour === currentHour) {
-      $(this).children(".description").addClass("present");
-    }
-    else {
-      $(this).children(".description").addClass("future");
-    }
+    // load event from local storage
+    var eventDescription = localStorage.getItem(timeBlockHour);
+
+    // display event in time block
+    $(this).children(".description").val(eventDescription);
   });
-}
-
-setTimeBlockColor();
-
-// save events to local storage
-$(".saveBtn").on("click", function() {
-  // get event description and hour
-  var eventDescription = $(this).siblings(".description").val();
-  var eventHour = $(this).siblings(".hour").text();
-
-  // save event to local storage
-  localStorage.setItem(eventHour, eventDescription);
-});
-
-// load events from local storage
-$(".time-block").each(function() {
-  // get time block hour
-  var timeBlockHour = $(this).children(".hour").text();
-
-  // load event from local storage
-  var eventDescription = localStorage.getItem(timeBlockHour);
-
-  // display event in time block
-  $(this).children(".description").val(eventDescription);
 });
