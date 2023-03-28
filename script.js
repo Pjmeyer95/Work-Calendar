148c38db08340e5c1eee4af9e331d4d382780dc9
pjmeyer95 <pjmeyer95@gmail.com>
2023-03-27 20:03:13 -0600

    commit

--- script.js
+++ script.js
@@ -1,54 +1,54 @@
-$(document).ready(function() {
-  // get current date
-  var currentDate = moment().format("dddd, MMMM Do");
+// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 
-  // display current date in header
-  $("#currentDay").text(currentDate);
-
-  // check time and set time block color
-  function setTimeBlockColor() {
-    // get current hour
-    var currentHour = moment().hour();
-
-    // loop through time blocks
-    $(".time-block").each(function() {
-      // get time block hour
-      var timeBlockHour = parseInt($(this).children(".hour").text().slice(0, -2));
-
-      // set time block color based on current hour
-      if (timeBlockHour < currentHour) {
-        $(this).children(".description").addClass("past");
-      }
-      else if (timeBlockHour === currentHour) {
-        $(this).children(".description").addClass("present");
-      }
-      else {
-        $(this).children(".description").addClass("future");
-      }
-    });
-  }
-
-  setTimeBlockColor();
-
-  // save events to local storage
-  $(".saveBtn").on("click", function() {
-    // get event description and hour
-    var eventDescription = $(this).siblings(".description").val();
-    var eventHour = $(this).siblings(".hour").text();
-
-    // save event to local storage
-    localStorage.setItem(eventHour, eventDescription);
+// the code isn't run until the browser has finished rendering all the elements
+// in the html.
+$(function () {
+  var currenthour = dayjs().hour();
+  var blockcontainer = $(".container-lg")
+  var storeitems =
+      JSON.parse(window.localStorage.getItem("storage"))
+  console.log(currenthour);
+  $(".saveBtn").on("click", function (event) {
+    event.preventDefault();
+    var hour = $(this).parent().attr("id").split("-")[1];
+    console.log(hour);
+    var tasks = $(this).siblings(".description").val();
+    console.log(tasks);
+    var savetostorage =
+      JSON.parse(window.localStorage.getItem("storage")) || [];
+    savetostorage.push({ hour, tasks });
+    window.localStorage.setItem("storage", JSON.stringify(savetostorage));
   });
 
-  // load events from local storage
-  $(".time-block").each(function() {
-    // get time block hour
-    var timeBlockHour = $(this).children(".hour").text();
-
-    // load event from local storage
-    var eventDescription = localStorage.getItem(timeBlockHour);
-
-    // display event in time block
-    $(this).children(".description").val(eventDescription);
-  });
+    for (let i = 9; i < 18; i++) {
+    var timeblock = $("<div>")
+   timeblock.addClass("row time-block") .attr("id","hour-"+i)
+   var hourblock = $("<div>")
+   hourblock.addClass("col-2 col-md-1 hour text-center py-3").text(dayjs().hour(i).format("h A"))
+   var textarea = $("<textarea>")
+   textarea.addClass ("col-2 col-md-1 hour text-center py-3").val
+   if (storeitems[i].hour==i) {
+    textarea.val(storeitems[i].tasks)
+   }
+    timeblock.append(hourblock).append(textarea)
+    timeblock.appendTo(blockContainer)
+   }
+  // TODO: Add a listener for click events on the save button. This code should
+  // use the id in the containing time-block as a key to save the user input in
+  // local storage. HINT: What does `this` reference in the click listener
+  // function? How can DOM traversal be used to get the "hour-x" id of the
+  // time-block containing the button that was clicked? How might the id be
+  // useful when saving the description in local storage?
+  //
+  // TODO: Add code to apply the past, present, or future class to each time
+  // block by comparing the id to the current hour. HINTS: How can the id
+  // attribute of each time-block be used to conditionally add or remove the
+  // past, present, and future classes? How can Day.js be used to get the
+  // current hour in 24-hour time?
+  //
+  // TODO: Add code to get any user input that was saved in localStorage and set
+  // the values of the corresponding textarea elements. HINT: How can the id
+  // attribute of each time-block be used to do this?
+  //
+  // TODO: Add code to display the current date in the header of the page.
 });
